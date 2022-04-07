e061e558b6cb795a6274267a9fceb9d55adc203b
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-07 18:34:44 +0300

    Update primer3.java

--- src/Example_3/primer3.java
+++ src/Example_3/primer3.java
@@ -1,2 +1,17 @@
-package Example_3;public class primer3 {
+package Example_3;
+
+public class primer3 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new RuntimeException("ошибка");
+        } catch (NullPointerException e) {
+            System.out.println("1" );
+        }catch (RuntimeException e) {
+            System.out.println("2" );
+        }catch (Exception e) {
+            System.out.println("3" );
+        }
+        System.out.println("4");
+    }
 }
