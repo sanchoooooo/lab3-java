15f7bda3ac146a1bb1a7fc9639b9c7ba9aeff822
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-07 18:37:42 +0300

    Update primer10.java

--- src/Example_10/primer10.java
+++ src/Example_10/primer10.java
@@ -1,2 +1,15 @@
-package Example_10;public class primer10 {
-}
+package Example_10;
+
+public class primer10 {
+    public static int m(){
+        try {
+            System.out.println("0");
+            return 15;
+        } finally {
+            System.out.println("1");
+            return 20;
+        } }
+    public static void main(String[] args) {
+        System.out.println(m());
+    }}
+
