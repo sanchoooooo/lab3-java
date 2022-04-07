4a5a43cae62bd2d2c019a3d0271818f7760e5af4
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-07 18:38:25 +0300

    Update primer12.java

--- src/Example_12/primer12.java
+++ src/Example_12/primer12.java
@@ -1,2 +1,14 @@
-package Example_12;public class primer12 {
-}
+package Example_12;
+
+public class primer12 {
+    public static void m(String str, double chislo){
+        if (str==null) {
+            throw new IllegalArgumentException("Строка введена неверно");
+        }
+        if (chislo>0.001) {
+            throw new IllegalArgumentException("Неверное число");
+        } }
+    public static void main(String[] args) {
+        m(null,0.000001);
+    }}
+
