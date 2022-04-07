eb439303c164425489f43ad92a84adb7d005bcc5
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-07 18:39:11 +0300

    Update primer14.java

--- src/Example_14/primer14.java
+++ src/Example_14/primer14.java
@@ -1,2 +1,16 @@
-package Example_14;public class primer14 {
-}
+package Example_14;
+
+public class primer14 {
+    public static void m(int x) throws ArithmeticException{
+        int h=10/x;
+    }
+    public static void main(String[] args) {
+        try {
+            int l = args.length;
+            System.out.println("размер массива= " + l);
+            m(l);
+        } catch (ArithmeticException e) {
+            System.out.println("Ошибка: Деление на ноль");
+        }
+    } }
+
