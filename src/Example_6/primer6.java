556635b8d8298a1bdaed2d15c59c318b383db207
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-07 18:35:50 +0300

    Update primer6.java

--- src/Example_6/primer6.java
+++ src/Example_6/primer6.java
@@ -1,2 +1,19 @@
-package Example_6;public class primer6 {
+package Example_6;
+
+public class primer6 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new NullPointerException("ошибка");
+        } catch (ArithmeticException e) {
+            System.out.println("1");
+        }catch (RuntimeException e) {
+            System.out.println("2");
+        } catch (Exception e) {
+            System.out.println("3");
+
+        }
+        System.out.println("4");
+    }
+
 }
