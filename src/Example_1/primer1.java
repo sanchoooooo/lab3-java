a5de1ff54c22e908151bd5f99da808054f3b1ada
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-07 18:33:58 +0300

    Update primer1.java

--- src/Example_1/primer1.java
+++ src/Example_1/primer1.java
@@ -1,2 +1,14 @@
-package Example_1;public class primer1 {
+package Example_1;
+
+public class primer1 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new RuntimeException("Непроверяемая ошибка");
+        } catch (RuntimeException e) { // исключение перехвачено
+            System.out.println("1"+ e); // исключение обработано
+        }
+        System.out.println("2");
+    }
 }
+
